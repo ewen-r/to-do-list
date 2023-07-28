da5e700326440a21621b6c320459dc2862ca4cb1
ewen-r <ewen.reynolds@gmail.com>
2023-07-28 22:23:42 +0100

    Load env vars. For convenience.. If env.USERNAME === 'EREYNOLDS' then also automatically load .env file. Exit if mongo uri is not found. Tweaks to debug. Fixed race-conditions due to not waiting for some db ops to finish. Added new scripts for npm... npm start:dev and start

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 
+// Load and check env.
+loadAndCheckEnv();
 
 /* Set dirname prefix for the current root so that files can be served.
  * e.g res.sendFile(_dirname + '/web/index.html'); */
@@ -12,9 +15,6 @@ const _dirname = dirname(fileURLToPath(import.meta.url));
 const PROJECT = "to-do-list-app";
 const app = express();
 const PORT = 3000;
-
-// Mongo db configuration
-const mongoDbUri = `mongodb://127.0.0.1/${PROJECT}`;
 const Schema = mongoose.Schema;
 const ObjectId = Schema.ObjectId;
 
@@ -36,11 +36,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Connect to MongoDB database
 async function connectToMongo() {
-  console.log(`connectToMongo(): Connecting to ${mongoDbUri}`);
+  console.log('connectToMongo(): Connecting to mongo');
 
   try {
-    await mongoose.connect(mongoDbUri);
-    console.log(`connectToMongo(): Connected to ${mongoDbUri}`);
+    await mongoose.connect(`${process.env.SECRET_MONGO_URI}/${PROJECT}`);
+    console.log('connectToMongo(): Connected to mongo');
 
   } catch (err) {
     console.error(`ERROR: connectToMongo(): ${err}`);
@@ -93,10 +93,10 @@ app.get('/:listName',
 
 // Handle POST requests to '/'
 app.post('/',
-  (req, res) => {
+  async function (req, res) {
     console.log('POST: "/"', req.body);
 
-    TaskModel(
+    await TaskModel(
       {
         listName: req.body.listName,
         text: req.body.newItem,
@@ -112,12 +112,12 @@ app.post('/',
 
 // Handle POST requests to '/done'
 app.post('/done/:listName/:_id',
-  (req, res) => {
+  async function (req, res) {
     console.log('POST: "/done"', req.params, req.body);
 
     // Find document and update the "done" field.
     const done = req.body?.done === 'on' ? true : false;
-    TaskModel.findByIdAndUpdate(req.params._id, { done: done }).exec();
+    await TaskModel.findByIdAndUpdate(req.params._id, { done: done }).exec();
     // Redirect back to the list.
     res.redirect(`/${req.params.listName}`);
   }
@@ -137,7 +137,35 @@ app.post('/prune',
   }
 );
 
+
+/** Load and check env.
+  * - If env.USERNAME === 'EREYNOLDS' ...
+  * - - Automatically load .env file.
+  * - Check for Mongo Uri and exit if not found.
+*/
+function loadAndCheckEnv() {
+  console.log('=========================');
+
+  // If env.USERNAME === 'EREYNOLDS' ...
+  if (process.env.USERNAME === 'EREYNOLDS') {
+    console.log('= RUNNING LOCALLY');
+    // Automatically load .env file.
+    dotenv.config();
+  } else {
+    console.log('= RUNNING PRODUCTION');
+  }
+
+  console.log('=========================');
+
+  // Check for Mongo Uri and exit if not found.
+  if (!process.env.SECRET_MONGO_URI) {
+    console.error('ERROR: Unable to load Mongo uri.');
+    process.exit(5);
+  }
+}
+
 //////////////////////////////////////////////////////////////////////////////////////////////
+// 
 
 // Connect to mongo database.
 connectToMongo();
@@ -145,6 +173,6 @@ connectToMongo();
 // Start the app, listening on PORT "PORT".
 app.listen(PORT,
   () => {
-    console.log(`PROJECT "${PROJECT}": Server is running on http://localhost:${PORT}`);
+    console.log(`PROJECT "${PROJECT}": Server is running on port ${PORT}`);
   }
 );
